8b0148f856636af0bfe5834b470fac218d955e41
Colin Weaver <colin@itdojo.com>
2024-02-27 18:42:20 -0500

    Updated shell-login-settings.sh

--- shell-login-settings.sh
+++ shell-login-settings.sh
@@ -3,26 +3,23 @@
 # My shell settings
 
 gitssh() {
-    local GITHUB_SSH_KEY="github-office2"
+    local github_ssh_key="github-office"
 
-    if [ -f ~/.ssh/"$GITHUB_SSH_KEY" ]; then
-        eval "$(ssh-agent)" &>/dev/null
-        ssh-add ~/.ssh/"$GITHUB_SSH_KEY" &>/dev/null
-        ssh -T git@github.com &>/dev/null
-        if [ $? -eq 1 ]; then
-            printf "%s\n" "❌  GitHub Authentication: Failed." >&2
-            return 1
-        else
-            printf "%s\n" "✅  GitHub Authentication: Success." >&2
-            return 0
-        fi
-        return 0
-    else
-        printf "%s\n" "⚠️  No SSH key named '$GITHUB_SSH_KEY' found." >&2
-        printf "%s\n" "Cannot authenticate to GitHub without SSH key." >&2
+    if [[ ! -f ~/.ssh/"$github_ssh_key" ]]; then
+        printf "%s\n" "⚠️  No SSH key named '$github_ssh_key' found." "Cannot authenticate to GitHub without SSH key." >&2
         return 1
     fi
-}   
+
+    eval "$(ssh-agent)" >/dev/null
+    ssh-add ~/.ssh/"$github_ssh_key" >/dev/null
+
+    if ! ssh -T git@github.com 2>&1 | grep -q "You've successfully authenticated"; then
+        printf "%s\n" "❌  GitHub Authentication: Failed." >&2
+        return 1
+    else
+        printf "%s\n" "✅  GitHub Authentication: Success." >&2
+    fi
+} 
 
 if [ "$(uname -s)" = "Linux" ]; then
     echo "Linux"
